ba61950a1a7bec278989c9db08c952ec587d01cd
Casey Doran <cdoran2011@my.fit.edu>
2018-12-16 14:39:22 -0800

    clamp simulation to defined extents (well, an extent)

--- sim/dynamics.py
+++ sim/dynamics.py
@@ -1,8 +1,9 @@
 class Dynamics:
-    def __init__(self, environment, tickiterations=50):
+    def __init__(self, environment, tickiterations=50, elasticcollisions=False):
         self.tickiterations = tickiterations
         self.env = environment
         self.objects = []
+        self.elasticcollisions = elasticcollisions
         self.time = 0.0
         print('Dynamics initialized.')
         print('bounds: ', self.env.extents())
@@ -26,6 +27,19 @@ class Dynamics:
                 obj.vel += self.env.gravity() * (dt / self.tickiterations)
                 # translate
                 obj.pos += obj.vel * (dt / self.tickiterations)
+
+                # clamp to bounds
+                # TODO: repeat for additional extents
+                if obj.pos.z < self.env.extents()['minZ']:
+                    obj.pos.z = self.env.extents()['minZ']
+                    if obj.vel.z < 0:
+                        if self.elasticcollisions:
+                            # elastic collision preserves kinetic energy
+                            obj.vel.z = -obj.vel.z
+                        else:
+                            # inelastic collision just kinda stops
+                            obj.vel.z = 0
+
             if callable(sample):
                 sample()
         self.printstatus()
